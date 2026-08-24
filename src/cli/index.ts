3688dfa3051694cfca5050ff8bddf1c4ac1fb4f7
miyago9267 <miyagomizugen@gmail.com>
2026-08-24 14:57:09 +0800

    refactor: 移除 CLI 與 TUI client surface

--- src/cli/index.ts
+++
@@ -1,4 +0,0 @@
-export { parseArgs, HELP_TEXT, type ParsedArgs, type PermissionMode } from './args.ts'
-export { runTui, type TuiOptions } from './tui.ts'
-export { buildSessionOptions } from './session-options.ts'
-export { c } from './colors.ts'
